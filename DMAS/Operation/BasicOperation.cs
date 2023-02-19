2a4e64f3c935c82429fd3fc4ee696e983aca0a8f
DarshanSudhakar <$K3Ug1i&>
2023-02-19 16:23:16 +0530

    Adding the basic opertions library and testing it using console application

---
+++ DMAS/Operation/BasicOperation.cs
@@ -0,0 +1,29 @@
+﻿namespace Operation
+{
+    public class BasicOperation
+    {
+        public int Add(int augend, int addend) 
+        { 
+            var sum = augend + addend;
+            return sum;
+        }
+
+        public int Sub(int minuend, int subtrahend)
+        {
+            var difference = minuend - subtrahend;
+            return difference;
+        }
+
+        public int Multiply(int multiplicand, int multiplier)
+        {
+            var product = multiplicand * multiplier;
+            return product;
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            var quotient = dividend / divisor;
+            return quotient;
+        }
+    }
+}
